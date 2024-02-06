5a10c5cf604a9eaf3b1b01714f71f6fe85de68c4
here-is-written-a-serious-username <lordbambycha93@gmail.com>
2024-02-06 13:27:07 +0200

    acc rotate

--- js/accordion.js
+++ js/accordion.js
@@ -16,3 +16,25 @@
 
 
 new Accordion('.accordion-container');
+
+// function changeSVG() {
+//     const svgIcon = document.getElementById('ac-trigger__icon');
+
+//     // Змініть атрибут "href" відповідно до вашого нового шляху SVG.
+//     svgIcon.setAttribute('href', './новий/шлях/до/вашого/нового/зображення.svg');
+// }
+
+const acTriggers = document.querySelectorAll('.ac-trigger');
+
+// acTriggers.forEach(trigger => {
+//     trigger.addEventListener('click', () => {
+//         const iconId = trigger.getAttribute('data-icon-id');
+//         const currentHref = document.getElementById(iconId).querySelector('use').getAttribute('href');
+
+//         // Замінюємо "icon+" на "iconNew"
+//         const newHref = currentHref.replace('icon+', 'icon-x');
+
+//         // Змінюємо href у вибраному SVG
+//         document.getElementById(iconId).querySelector('use').setAttribute('href', newHref);
+//     });
+// });
